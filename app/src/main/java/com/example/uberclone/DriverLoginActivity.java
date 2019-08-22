ed3a44ba35952b9860a828fc2dd469a95ee91df6
masum43 <masum.abir43@gmail.com>
2019-08-22 23:56:44 +0600

    Registration via firebase Authentication

--- app/src/main/java/com/example/uberclone/DriverLoginActivity.java
+++ app/src/main/java/com/example/uberclone/DriverLoginActivity.java
@@ -1,14 +1,101 @@
 package com.example.uberclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DriverLoginActivity extends AppCompatActivity {
+
+    private EditText mEmail,mPassword;
+    private Button mLogin,mRegistrartion;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser user = mAuth.getCurrentUser();
+                if(user != null)
+                {
+                    Intent intent = new Intent(DriverLoginActivity.this,DriverLoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return;
+                }
+
+            }
+        };
+
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+
+        mLogin = findViewById(R.id.loginBtn);
+        mRegistrartion = findViewById(R.id.registrationBtn);
+
+        mRegistrartion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final String emailStr = mEmail.getText().toString();
+                final String passStr = mPassword.getText().toString();
+
+                mAuth.createUserWithEmailAndPassword(emailStr,passStr).addOnCompleteListener(DriverLoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(!task.isSuccessful())
+                        {
+                            Toast.makeText(DriverLoginActivity.this, "error occurred....", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            String user_id = mAuth.getCurrentUser().getUid();
+                            DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child("Drivers").child(user_id);
+                            current_user_db.setValue(true);
+                        }
+
+
+                    }
+                });
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        mAuth.addAuthStateListener(firebaseAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(firebaseAuthListener);
     }
 }
